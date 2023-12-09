08b2795b9401f1073d59701628a63735717afad7
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-12-10 01:41:11 +0530

    add more settings

--- server/settings.py
+++ server/settings.py
@@ -133,3 +133,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_URL = 'static/'
+STATICFILES_DIR = os.path.join(BASE_DIR, 'uploads')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'uploads')
